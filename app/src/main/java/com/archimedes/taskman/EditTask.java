bbd29f8b3eb0c2ee1835f2ec5fa4154a77957fcb
Richard Djirackor <rdjirackor@st.ug.edu.gh>
2023-08-18 18:48:50 +0000

    Implemented mechanism for saving task name, task description and seek bar value

--- app/src/main/java/com/archimedes/taskman/EditTask.java
+++ app/src/main/java/com/archimedes/taskman/EditTask.java
@@ -1,76 +1,103 @@
 package com.archimedes.taskman;
+/**
+ In this code, I am basically receiving the selected priority rom the user from the seekbar,
+ and then I want to store that priority, but then Anytime I store the priority and tehn on another instance tyeh user does the same thing
+ I dont want the previous priority to be overwritten and so I initialiazed, a variable which I cakled Are, which will
+ basically be added to the priority, to distinguish it from the prevoius. I will optimize this oce I get better, hey..
+ *
+ */
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-//TODO: Design the Pages
 
 public class EditTask extends AppCompatActivity {
-    int PRIORITY = 1;
-
-    /**
-     In this code, I am basically receiving the selected priority rom the user from the seekbar,
-     and then I want to store that priority, but then Anytime I store the priority and tehn on another instance tyeh user does the same thing
-     I dont want the prevoius priority to be overwritten and so I initialiazed, a variable which I cakled Are, which will
-     basically be added to the priority, to distinguish it from the prevoius. I will optimize this oce I get better, hey..
-     Here's a TODO: OPTIMIZE Are variable.
-     To remind me to simplify it
-     *
-     */
+    int priority = 1;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    int count;
+    private EditText editName, editDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_task);
-        SharedPreferences preferences= getSharedPreferences("SavedStuff", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        int Count = 0;
-        editor.putInt("count", Count);
-        editor.apply();
+
+        // Initialize SharedPreferences
+        preferences = getSharedPreferences("SavedStuff", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        count = preferences.getInt("count", 0);
 
         SeekBar seekerSensitive = findViewById(R.id.seekBar);
         Button saveTask = findViewById(R.id.saveTask);
-        saveTask.setOnClickListener(new View.OnClickListener() {
+        editName = findViewById(R.id.editN);
+        editDesc = findViewById(R.id.editD);
+
+        Button test =findViewById(R.id.TPL);
+
+        String TaskName=editName.getText().toString();
+        String TaskDesc=editDesc.getText().toString();
+
+
+        seekerSensitive.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-                seekerSensitive.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                    int Are=preferences.getInt("count",0);
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        switch (progress) {
-                            case 0:
-                                PRIORITY=0;
-                                break;
-                            case 2:
-                                PRIORITY=2;
-                                break;
-                            case 1:
-                            default:
-                                PRIORITY=1;
-                        }
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch (progress) {
+                    case 0:
+                        priority = 0;
+                        break;
+                    case 2:
+                        priority = 2;
+                        break;
+                    case 1:
+                    default:
+                        priority = 1;
+                }
+            }
 
-                        editor.putString("Priority"+Are, String.valueOf(PRIORITY));
-                        editor.apply();
-                        Are++;
-                    }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Not needed for this example
+            }
 
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-
-                    }
-                });
-
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Not needed for this example
             }
         });
 
+        saveTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putString("Priority" + String.valueOf(count), String.valueOf(priority));
+                editor.putString("TaskName" + String.valueOf(count), TaskName);
+                editor.putString("TaskDesc" + String.valueOf(count), TaskDesc);
+
+                count++;
+                editor.putInt("count", count);
+                editor.commit(); // Use commit instead of apply
+            }
+        });
+       /** test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String a=preferences.getString("Priority0","None yet!");
+                String b=preferences.getString("TaskName0","None yet!");
+                String c=preferences.getString("TaskDesc0","None yet!");
+                String a1=preferences.getString("Priority1","None yet!");
+                String b1=preferences.getString("TaskName1","None yet!");
+                String c1=preferences.getString("TaskDesc1","None yet!");
+                String enigma = a+b+c;
+                String enigma1 = a1+b1+c1;
+                Toast.makeText(EditTask.this, enigma+"\n"+enigma1, Toast.LENGTH_SHORT).show();
+            }
+        });**/
     }
 }
